aea0a1793bc138e62a5d2ce29a67eadbd33237d7
selimtan <stanoncloud@gmail.com>
2022-12-10 14:34:51 +0300

    .

--- src/MyView.svelte
+++ src/MyView.svelte
@@ -1,14 +1,25 @@
 <script>
-	import { title, comicSans, box } from './css.js';
-	let count = 0;
+	import Thing from './Thing.svelte';
+
+	let things = [
+		{ id: 1, name: 'apple' },
+		{ id: 2, name: 'banana' },
+		{ id: 3, name: 'carrot' },
+		{ id: 4, name: 'doughnut' },
+		{ id: 5, name: 'egg' },
+	];
 
 	function handleClick() {
-		count += 1;
+		things = things.slice(1);
 	}
 </script>
 
-<div>Test</div>
-<div>Son Test</div>
-<button class={box} on:click={handleClick}>
-	Clicked {count} {count === 1 ? 'time' : 'times'}
+<div>test</div>
+<div>test</div>
+<button on:click={handleClick}>
+	Remove first thing
 </button>
+
+{#each things as thing}
+	<Thing name={thing.name}/>
+{/each}
